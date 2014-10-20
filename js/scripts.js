079bdc4018742099b2510d161c5f872749d6d708
gtabbert <gtabbert@eagles.ewu.edu>
2014-10-20 13:21:42 -0700

    version 0

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  // Your Scripts
+  $('#one').corner('30px');
+  $('#two').corner("bevel");
+  $('#three').corner("dog");
+
+
+
+});
